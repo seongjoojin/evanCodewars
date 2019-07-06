9ba3325b49afed7eb288bf8a0348d7fae9945e66
Seongjoo Jin <evanjin8@gmail.com>
2019-07-07 02:18:13 +0900

    day7 설명 추가

--- digitalRoot.js
+++ digitalRoot.js
@@ -1,4 +1,8 @@
-/* */
+/*
+1. 문자열로 만들고 나누어서 배열로 저장
+2. 배열에서 더한 후 더한 수를 다시 문자열로 만들어서 나누고 배열로 저장
+3. 더한 배열의 길이가 1이 되면 값을 return 시킴
+*/
 function sumArray(array) {
     let count = 0
     return array.map(item => {
